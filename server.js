2d89d927d6a6f18d87611bddd4703b5d2c22fad1
Rodrigo Rocha <rodrigorgs@gmail.com>
2025-05-16 22:50:40 -0300

    Updates

---
+++ server.js
@@ -0,0 +1,51 @@
+import fs from "fs";
+import https from "https";
+import express from "express";
+import { WebSocketServer } from "ws";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const server = https.createServer(
+  {
+    key: fs.readFileSync("cert.key"),
+    cert: fs.readFileSync("cert.crt"),
+  },
+  app
+);
+
+app.use(express.static(__dirname));
+
+const wss = new WebSocketServer({ server });
+let sender = null;
+const receivers = new Map();
+
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    const parsed = JSON.parse(msg);
+
+    if (parsed.role === "sender") {
+      sender = ws;
+    } else if (parsed.role === "receiver") {
+      receivers.set(parsed.id, ws);
+      sender?.send(JSON.stringify({ type: "connect", id: parsed.id }));
+    }
+
+    if (parsed.type && parsed.id) {
+      const target = ws === sender ? receivers.get(parsed.id) : sender;
+      if (target) target.send(JSON.stringify(parsed));
+    }
+  });
+
+  ws.on("close", () => {
+    receivers.forEach((rws, id) => {
+      if (rws === ws) receivers.delete(id);
+    });
+  });
+});
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("✅ HTTPS server running at https://0.0.0.0:3000");
+});
